cc7a23f6da56793cfce26daf5db30804e6473768
Sandu Curagau <sandu.curagau@hotmai.com>
2022-10-24 14:24:24 +0100

    Installed packages. Created basic project structure. Added basic routing with lazy loading.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
