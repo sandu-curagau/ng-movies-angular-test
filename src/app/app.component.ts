769189a89424d451ad8e4a3f6cff5c2966894422
Sandu Curagau <sandu.curagau@hotmai.com>
2022-10-25 03:44:59 +0100

    Implemented navbar. Changed internacionalization architecture and added pt language.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { NavbarEventTypes } from './enums/event-types.enum';
+import { NavbarEvent } from './models/output-events.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-movies';
+
+  supportedLanguages = ['en', 'es', 'pt'];
+  selectedLanguage: string;
+
+  constructor(private translateService: TranslateService) {
+    this.translateService.addLangs(this.supportedLanguages);
+    this.translateService.setDefaultLang('en');
+    const browserLang = this.translateService.getBrowserLang();
+    this.selectedLanguage = browserLang ? browserLang : 'en';
+    this.translateService.use(browserLang ? browserLang : 'en');
+  }
+
+  navbarEvent(event: NavbarEvent): void {
+    switch(event.eventType) {
+      case NavbarEventTypes.LanguageChanged:
+        this.selectedLanguage = event.data;
+        this.translateService.use(this.selectedLanguage);
+        break;
+      default:
+        break;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { RouterLinkActive } from '@angular/router';
 
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// These are imported in the app module because they're used in the navbar, which is permanently viewable throughout the whole app at all times
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms'
+import { SidebarModule } from 'primeng/sidebar';
 
 import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
@@ -36,7 +45,20 @@ import { interceptorProviders } from './interceptors/interceptors';
       maxAge: 25,
       logOnly: environment.production
     }),
-    EffectsModule.forRoot([])
+    EffectsModule.forRoot([]),
+    TranslateModule.forRoot(
+      {
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (http: HttpClient) => { return new TranslateHttpLoader(http, './assets/i18n/', '.json'); },
+          deps: [HttpClient]
+        }
+      }
+    ),
+    ButtonModule,
+    DropdownModule,
+    FormsModule,
+    SidebarModule
   ],
   providers: [
     interceptorProviders,
